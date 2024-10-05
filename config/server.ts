b34357f8651b1c1026603bfe9725f10599fe4af8
Bryan García <bryan1997st@gmail.com>
2024-10-05 12:31:54 -0500

    :zap: Adds new schema structure for blogs, authors and blog tags

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,15 @@
+
+const assignProductionUrl = ({ env }) => {
+  const urlProduction = env('NODE_ENV') === "production" ? {
+    url: "https://api.bryanstgarcia.com",
+  } : {}
+  return urlProduction
+}
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: "https://api.bryanstgarcia.com",
+  ...assignProductionUrl({ env }),
   app: {
     keys: env.array('APP_KEYS'),
   },
