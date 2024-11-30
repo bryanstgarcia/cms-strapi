b9898bb3e84adc0cdc8604ac9b525e5b8466617b
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-11-29 22:57:22 -0400

    🩹 Change routes privacy

--- src/api/blog/routes/_custom.ts
+++ src/api/blog/routes/_custom.ts
@@ -4,17 +4,11 @@ export default {
       method: "GET",
       path: "/blogs/:slug",
       handler: "blog.findOne",
-      config: {
-        auth: false,
-      },
     },
     {
       method: "GET",
       path: "/blogs/filter/:search",
       handler: "blog.customSearch",
-      config: {
-        auth: false,
-      },
     },
   ],
 };
